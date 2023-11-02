43e593d84b493e401c7e4edebb15174632d4409f
oakpunonu <147940216+oakpunonu@users.noreply.github.com>
2023-11-02 08:15:44 +0100

    Add files via upload

---
+++ week-2/practice-1.rs
@@ -0,0 +1,12 @@
+fn main() {
+	println!("Welcome to COS 101");
+
+    println!("The course learning outcomes is as follows:");
+
+    println!("1. Distinguish between computer and computer programming,/n
+    	      2. Develop some techniqes in computer science,/n
+    	      3. Understand the different areas of study in computer science,/n
+    	      4. Convergent with applications of computer science,/n
+    	      5. Navigate the career prospects in computer science,/n
+    	      6. Conversant with computer programming concepts.");
+}
