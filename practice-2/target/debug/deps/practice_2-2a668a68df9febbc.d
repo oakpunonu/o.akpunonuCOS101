b09802b51ec061ae392acc13e29cd4834b585d52
oakpunonu <147940216+oakpunonu@users.noreply.github.com>
2023-11-07 06:31:35 +0100

    Add files via upload

---
+++ practice-2/target/debug/deps/practice_2-2a668a68df9febbc.d
@@ -0,0 +1,5 @@
+/Users/ugochukwu/o.akpunonuCOS101/week-3/practice-2/target/debug/deps/practice_2-2a668a68df9febbc: src/main.rs
+
+/Users/ugochukwu/o.akpunonuCOS101/week-3/practice-2/target/debug/deps/practice_2-2a668a68df9febbc.d: src/main.rs
+
+src/main.rs:
---
+++ practice-2/target/debug/practice-2.d
@@ -0,0 +1,1 @@
+/Users/ugochukwu/o.akpunonuCOS101/week-3/practice-2/target/debug/practice-2: /Users/ugochukwu/o.akpunonuCOS101/week-3/practice-2/src/main.rs
